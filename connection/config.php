a28967d9fee97d0614e3725ab11723248ea11db3
Pedro Henrique Pinheiro Gomes <pedro@ipb.org.br>
2022-02-24 13:06:42 -0300

    connection/config.php alterado

--- connection/config.php
+++ connection/config.php
@@ -7,7 +7,7 @@ define('DB_DRIVER' , 'sqlsrv');
 define('DB_NAME' , 'testes');
 
 //nome do servidor (localhost, ita-homologacao, ita-clusersql, ...)
-define('DB_HOST' , 'ita-homologacao');
+define('DB_HOST' , 'asd');
 
 //porta utilizada para a conexao
 //Porta padrão SQL SERVER = 1433 ou 1434
@@ -15,7 +15,7 @@ define('DB_HOST' , 'ita-homologacao');
 define('DB_PORT' , '1433');
 
 //nome do usuario para logar no banco de dados
-define('DB_USER','depin');
+define('DB_USER','asd');
 
 //senha do usuario
-define('DB_PASSWD', 'niped.');
+define('DB_PASSWD', 'ads');
